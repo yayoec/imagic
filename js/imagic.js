d8f25a64d51905dddd9c46068d2673b3435a33cc
yayoec <yayoec@gmail.com>
2013-12-09 18:20:14 +0800

    add rotate

--- js/imagic.js
+++ js/imagic.js
@@ -2,6 +2,10 @@
 	var imgs = document.getElementsByTagName('html')[0].innerHTML.match(/<img[^>]*rel="__imagic"[^>]*?>/g);
 	var imglen = imgs.length;
 	var whs = [];
+	var userAgent = navigator.userAgent,
+		isIE = /msie/i.test(userAgent) && !window.opera,
+		isWebKit = /webkit/i.test(userAgent),
+		isFirefox = /firefox/i.test(userAgent);
 	for (var i=0; i<imglen; i++) {
 		var tmpImg = new Image();
 		tmpImg.src = parseDom(imgs[i]).src;
@@ -105,6 +109,24 @@
 			cover_dom.style.display = 'none';
 		}catch(e){
 			alert(e);
+		}
+	}
+
+	function rotate(target, degree) {
+		if (isWebKit) {
+			target.style.webkitTransform = "rotate(" + degree + "deg)";
+		} else if (isFirefox) {
+			target.style.MozTransform = "rotate(" + degree + "deg)";
+		} else if (isIE) {
+			//chessDiv.style.filter = "progid:DXImageTransform.Microsoft.BasicImage(rotation=" + degree + ")";
+			degree = degree / 180 * Math.PI;
+			var sinDeg = Math.sin(degree);
+			var cosDeg = Math.cos(degree);
+			 
+			target.style.filter = "progid:DXImageTransform.Microsoft.Matrix(" +
+					"M11=" + cosDeg + ",M12=" + (-sinDeg) + ",M21=" + sinDeg + ",M22=" + cosDeg + ",SizingMethod='auto expand')";
+		} else {
+			target.style.transform = "rotate(" + degree + "deg)";
 		}
 	}
 })()
